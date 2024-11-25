030b33a33a7db4ac74e20f9f69d089bbe670c9fa
saheeb_tareque <saheebtareque97@gmail.com>
2024-11-25 21:52:19 +0600

    Torch basics

--- Torch_basics.py
+++ Torch_basics.py
@@ -1,3 +1,41 @@
 import torch
-x = torch.rand(1)
-print(x)
+import numpy as np
+x = torch.ones(2)
+# print(x)
+
+y = torch.tensor([2.5,3])
+print(y)
+
+# z = x+y
+z = torch.add(x,y)
+print(z)
+
+y.add_(x)
+print(y)
+
+subtraction = torch.sub(x,y)
+print(subtraction)
+
+multiplication = torch.mul(x,y)
+print(multiplication)
+
+q= torch.rand(4,4)
+print(q)
+print(q[:,2])
+print(q[2,3])
+
+w = q.view(-1,8)
+print(w)
+
+# Torch to numpy
+a= torch.ones(5)
+b = a.numpy()
+print(type(b))
+
+
+#Numpy to Torch
+d = np.ones(2)
+g = torch.from_numpy(d)
+print(type(g))
+
+
